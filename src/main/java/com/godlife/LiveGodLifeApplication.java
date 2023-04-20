9100300eae82b7aaa3dcf7cf97e751765191e14c
fDevJc <fdevjc@gmail.com>
2023-04-20 16:41:28 +0900

    init commit

---
+++ src/main/java/com/godlife/LiveGodLifeApplication.java
@@ -0,0 +1,13 @@
+package com.godlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveGodLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveGodLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godlife/LiveGodLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveGodLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
